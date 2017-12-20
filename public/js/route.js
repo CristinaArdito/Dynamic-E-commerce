f586ed04942377e0bdbbf131bb7c7f85a3e04cf4
Kri <cristina.ardito@studenti.unicam.it>
2017-12-20 12:29:16 +0100

    Inserito ui-router, iniziato menù index

--- public/js/route.js
+++ public/js/route.js
@@ -1,14 +1,37 @@
 angular.module('myApp.controllers', []); 
 
-angular.module('kriapp', ['myApp.controllers', 'ngRoute'])
+angular.module('kriapp', ['myApp.controllers', 'ngRoute', 'ui.router'])
 .run(function(){
 
 })
-.config(function($routeProvider) {
+.config(function($routeProvider, $stateProvider) {
         $routeProvider
 
             // route for the home page
             .when('/', {
                 templateUrl : 'views/front-end/home.ejs',
-            });
-});            
+            })     
+
+
+/***** ROUTE VIEW UI *****/
+		$stateProvider
+
+        .state('home', {
+            url: '/',
+            templateUrl: 'views/indexLogout.ejs',
+            controller: 'mainController'
+        })
+        .state('loggedHome', {
+            url: '/',
+            templateUrl: 'views/indexLogin.ejs',
+            controller: 'mainController'
+        });
+	})        
+
+    .controller('MyCtrl', ['$state', function($state) {
+    }])
+
+    //Silenzia errori di codice del router delle view, non è influente per il funzionamento
+    .config(['$qProvider', function ($qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+        }]);    
