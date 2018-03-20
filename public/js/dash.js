c8143d0fad89195221cfa1f70d643647300989ea
Kri <cristina.ardito@studenti.unicam.it>
2018-03-20 18:44:48 +0100

    Home dashboard (wip)

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,6 +1,30 @@
 angular.module('myApp.controllers')
-.controller("werehouseController", ['$scope', '$compile', '$location',
-function ($scope, $compile, $location) {
+.controller("werehouseController", ['$scope', '$compile', '$location', 'productService',
+function ($scope, $compile, $location, productService) {
+
+	$scope.showProducts = function() {
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }])
 .controller("dashController", ['$scope', 'dataHandler' , function($scope, dataHandler){
     
